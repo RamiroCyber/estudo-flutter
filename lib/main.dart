5d4aa5c7d2f7fcd74661476d16f2ec2f7b6e7853
RamiroCyber <ramiro.dr@gmail.com>
2022-07-31 22:51:02 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,90 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
 
-void main(){
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  void decrement() {
+    print("Decremento");
+  }
+
+  void increment() {
+    print("increment");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Pode entrar !",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text(
+            "0",
+            style: TextStyle(
+              fontSize: 100,
+              color: Colors.white,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: increment,
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(90, 90),
+                  primary: Colors.black
+                ),
+                child: const Text(
+                  "Entrou",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: decrement,
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(90,90),
+                  primary: Colors.black
+                ),
+                child: const Text(
+                  "Saiu",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
